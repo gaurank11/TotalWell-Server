ded55ae4df400b011ab26faa455838b0abc1fd5d
gaurank11 <sharmagaurank63@gmail.com>
2024-11-11 16:46:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const { Server } = require('socket.io');
+
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Socket.IO server running...');
+});
+
+// Initialize Socket.IO with the HTTP server
+const io = new Server(server, {
+  cors: {
+    origin: "*",  // Allow all origins or specify your frontend's URL
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on('connection', (socket) => {
+  console.log(`Socket Connected: ${socket.id}`);
+
+  socket.on('room:join', (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit('user:joined', { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit('room:join', data);
+  });
+
+  socket.on('user:call', ({ to, offer }) => {
+    io.to(to).emit('incomming:call', { from: socket.id, offer });
+  });
+
+  socket.on('call:accepted', ({ to, ans }) => {
+    io.to(to).emit('call:accepted', { from: socket.id, ans });
+  });
+
+  socket.on('peer:nego:needed', ({ to, offer }) => {
+    console.log('peer:nego:needed', offer);
+    io.to(to).emit('peer:nego:needed', { from: socket.id, offer });
+  });
+
+  socket.on('peer:nego:done', ({ to, ans }) => {
+    console.log('peer:nego:done', ans);
+    io.to(to).emit('peer:nego:final', { from: socket.id, ans });
+  });
+});
+
+// Start the server
+server.listen(8000, () => {
+  console.log('Server is running on http://localhost:8000');
+});
